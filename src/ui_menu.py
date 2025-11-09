252bcbf01b415b2a1454cb020e3257c557921eb2
BlueSoheil99 <soheil.k.78@gmail.com>
2025-11-08 17:23:01 -0800

    updated setupdialog to include nearmiss analysis

--- src/ui_menu.py
+++ src/ui_menu.py
@@ -1,9 +1,9 @@
 from PyQt6.QtWidgets import (
     QDialog, QVBoxLayout, QPushButton, QFileDialog,
     QLabel, QHBoxLayout, QCheckBox, QDateEdit, QTimeEdit,
-    QComboBox
+    QComboBox, QStackedLayout, QWidget, QMessageBox
 )
-from PyQt6.QtCore import QDate, QTime
+from PyQt6.QtCore import QDate, QTime, Qt
 
 
 class SetupDialog(QDialog):
@@ -11,30 +11,75 @@ class SetupDialog(QDialog):
         super().__init__()
         self.setWindowTitle("Setup Intersection")
 
-        layout = QVBoxLayout(self)
+        self.main_layout = QVBoxLayout(self)
 
         self.video_paths = []
+        self.data_path = None
+
+        # --- Dropdown menu ---
+        collection_types = ["Volume only", 'Near miss evaluation', 'find conflicts']
+        self.main_layout.addWidget(QLabel("Select data collection type:"))
+        self.collection_type = QComboBox()
+        self.collection_type.addItems(collection_types)
+        self.main_layout.addWidget(self.collection_type)
+        self.collection_type.currentIndexChanged.connect(self._switch_type)
 
         # Select video button
         self.video_btn = QPushButton("Select Videos")
-        self.video_btn.clicked.connect(self.select_videos)
-        layout.addWidget(self.video_btn)
+        self.video_btn.clicked.connect(self._select_videos)
+        self.main_layout.addWidget(self.video_btn)
 
+        self.type_stack = QStackedLayout()
+        ##### for volume data collection
+        self.volume_widget = self._get_volume_config_widget()
+        self.type_stack.addWidget(self.volume_widget)
+        ##### for conflict
+        self.data_btn = QPushButton("Select metadata")
+        self.data_btn.clicked.connect(self._select_data)
+        # self.type_stack.addWidget(self.data_btn)
+        data_widget = QWidget()
+        data_layout = QVBoxLayout(data_widget)
+        data_layout.addWidget(self.data_btn, alignment=Qt.AlignmentFlag.AlignTop)  # no stretching
+        self.type_stack.addWidget(data_widget)
+        ### add stack to the main layout
+        self.main_layout.addLayout(self.type_stack)
+
+
+        # OK/Cancel
+        buttons_layout = QHBoxLayout()
+        ok_btn = QPushButton("OK")
+        cancel_btn = QPushButton("Cancel")
+        ok_btn.clicked.connect(self._click_ok)
+        cancel_btn.clicked.connect(self.reject)
+        buttons_layout.addWidget(ok_btn)
+        buttons_layout.addWidget(cancel_btn)
+        self.main_layout.addLayout(buttons_layout)
+
+    def _is_collection_type_volume(self):
+        return self.collection_type.currentText() == "Volume only"
+
+    def _switch_type(self):
+        if self._is_collection_type_volume():
+            self.type_stack.setCurrentIndex(0)
+        else:
+            self.type_stack.setCurrentIndex(1)
+
+    def _get_volume_config_widget(self):
+        volume_widget = QWidget(self)
+        volume_layout = QVBoxLayout(volume_widget)
         # --- Observation date ---
         self.date_input = QDateEdit()
         self.date_input.setCalendarPopup(True)
         self.date_input.setDate(QDate.currentDate())
-        layout.addWidget(QLabel("Date of observation:"))
-        layout.addWidget(self.date_input)
-
+        volume_layout.addWidget(QLabel("Date of observation:"))
+        volume_layout.addWidget(self.date_input)
         # --- Video start time ---
         self.time_input = QTimeEdit()
         self.time_input.setTime(QTime(12, 0))
-        layout.addWidget(QLabel("Video starting time:"))
-        layout.addWidget(self.time_input)
-
+        volume_layout.addWidget(QLabel("Video starting time:"))
+        volume_layout.addWidget(self.time_input)
         # Approaches
-        layout.addWidget(QLabel("Select approaches:"))
+        volume_layout.addWidget(QLabel("Select approaches:"))
         self.approaches = {}
         approaches_layout = QHBoxLayout()
         for name in ["NB", "SB", "WB", "EB"]:
@@ -42,26 +87,34 @@ class SetupDialog(QDialog):
             cb.setChecked(True)
             self.approaches[name] = cb
             approaches_layout.addWidget(cb)
-        layout.addLayout(approaches_layout)
-
-        # --- Dropdown menu ---
-        layout.addWidget(QLabel("Select data collection type:"))
-        self.collection_type = QComboBox()
-        self.collection_type.addItems(["Volume only", "speed only", "Volume and Speed", 'Near miss'])
-        layout.addWidget(self.collection_type)
-
-        # OK/Cancel
-        buttons_layout = QHBoxLayout()
-        ok_btn = QPushButton("OK")
-        cancel_btn = QPushButton("Cancel")
-        ok_btn.clicked.connect(self.accept)
-        cancel_btn.clicked.connect(self.reject)
-        buttons_layout.addWidget(ok_btn)
-        buttons_layout.addWidget(cancel_btn)
-        layout.addLayout(buttons_layout)
+        volume_layout.addLayout(approaches_layout)
+        return volume_widget
 
 
-    def select_videos(self):
+    def _click_ok(self):
+        # if len(self.video_paths)==0:
+        if not self.video_paths:
+            self._run_msg_box("Missing Videos",
+                              "Please select at least one video before proceeding.")
+            return
+        if  self._is_collection_type_volume():
+            self.accept()
+        else:
+            if self.data_path is None:
+                self._run_msg_box("Missing Metadata",
+                                  "Please select the metadata file before proceeding.")
+                return
+            self.accept()
+
+    def _run_msg_box(self, title, txt):
+        msg = QMessageBox(self)
+        msg.setIcon(QMessageBox.Icon.Warning)
+        msg.setWindowTitle(title)
+        msg.setText(txt)
+        msg.setStandardButtons(QMessageBox.StandardButton.Ok)
+        msg.exec()
+
+    def _select_videos(self):
         paths, _ = QFileDialog.getOpenFileNames(self, "Select Video")
         if paths:
             self.video_paths = paths
@@ -69,18 +122,34 @@ class SetupDialog(QDialog):
             self.video_btn.setText(f'{len(paths)} video(s) selected')
         print(self.video_paths)
 
+    def _select_data(self):
+        path, _ = QFileDialog.getOpenFileName(self, "Select Metadata")
+        if path:
+            self.data_path = path
+            # self.video_btn.setText(paths.split("/")[-1])
+            self.data_btn.setText(f'Metadata: {path.split("/")[-1]} selected')
+        print(self.data_path)
+
+
     def get_config(self):
         """Return dict with selected approaches, date, and start time."""
-        selected_approaches = [name for name, cb in self.approaches.items() if cb.isChecked()]
-        t = self.get_start_time()
-        return {
-            "video_paths": self.video_paths,
-            "collection_type": self.collection_type.currentText(),
-            "approaches": selected_approaches,
-            "date": self.get_date(),
-            "start_time": t[0],
-            "start_time_seconds": t[1]
-        }
+        if self._is_collection_type_volume():
+            selected_approaches = [name for name, cb in self.approaches.items() if cb.isChecked()]
+            t = self.get_start_time()
+            return {
+                "video_paths": self.video_paths,
+                "collection_type": self.collection_type.currentText(),
+                "approaches": selected_approaches,
+                "date": self.get_date(),
+                "start_time": t[0],
+                "start_time_seconds": t[1]
+            }
+        else:
+            return {
+                "video_paths": self.video_paths,
+                "data_path": self.data_path,
+                "collection_type": self.collection_type.currentText()
+            }
 
 
     def get_date(self):
